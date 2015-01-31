c31eb04c0b7cbf8430d4e1c9b7740b4689401ecd
Nick <nick@stabtechs.com>
2015-01-31 10:20:41 -0800

    not working

--- src/main/scala/logic/Main.scala
+++ src/main/scala/logic/Main.scala
@@ -3,10 +3,10 @@ package logic
 import scala.scalajs.js
 import js.annotation.JSExport
 import scala.scalajs.js.Dynamic.{global => g}
+
 @JSExport
 object Main {
   @JSExport
   def main(): Unit = {
-    println("Working?")
   }
 }
