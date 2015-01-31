41f850f93825fb0187a7b60f0536c971c6d08969
Nick <nick@stabtechs.com>
2015-01-31 09:55:43 -0800

    not working

---
+++ generated/exports.js
@@ -0,0 +1,3 @@
+module.exports = function() {
+  logic.Main().main();
+}
--- src/main/scala/logic/Main.scala
+++ src/main/scala/logic/Main.scala
@@ -2,7 +2,7 @@ package logic
 
 import scala.scalajs.js
 import js.annotation.JSExport
-
+import scala.scalajs.js.Dynamic.{global => g}
 @JSExport
 object Main {
   @JSExport
