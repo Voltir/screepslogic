c65822ecfdecad4f52464bc4f2cab2a3ff232847
Nick <nick@stabtechs.com>
2015-02-01 10:19:18 -0800

    another attempt

--- src/main/scala/logic/Main.scala
+++ src/main/scala/logic/Main.scala
@@ -2,15 +2,9 @@ package logic
 
 import scala.scalajs.js
 import js.annotation.JSExport
-import scala.scalajs.js.Dynamic.{global => g}
 
-object ScreepGlobalScope extends js.GlobalScope {
-  def module:js.Dynamic = js.native
-}
-
+@JSExport("module.exports")
 object Main {
-
+  @JSExport
   def doIt(): Int = 2+2
-
-  ScreepGlobalScope.module.foo = doIt
 }
