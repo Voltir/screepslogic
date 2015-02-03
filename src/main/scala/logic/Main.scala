c60b6c0048440263fa8cf37400414a9a9a468514
Nick <nick@stabtechs.com>
2015-02-02 19:28:52 -0800

    blah

--- src/main/scala/logic/Main.scala
+++ src/main/scala/logic/Main.scala
@@ -2,9 +2,10 @@ package logic
 
 import scala.scalajs.js
 import js.annotation.JSExport
-
+import js.Dynamic.{ global => g }
 @JSExport("module.exports")
 object Main {
+  g.console.log("OMG WAT?")
   @JSExport
   def doIt(): Int = 2+2
 }
